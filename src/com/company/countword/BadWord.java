ae3c0941151027954a33f21f5ac5f4b1f7f8e9ac
Vlad-Andr <andreev.v99@gmail.com>
2019-07-31 00:23:37 +0300

    FixedAll

---
+++ src/com/company/countword/BadWord.java
@@ -0,0 +1,15 @@
+package com.company.countword;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BadWord {
+    static List<String> badWords = new ArrayList<>();
+
+    static
+    {
+        badWords.add("shit");
+        badWords.add("whore");
+        badWords.add("fuck");
+    }
+}
---
+++ src/com/company/countword/ReadSongText.java
@@ -0,0 +1,29 @@
+package com.company.countword;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ReadSongText {
+
+    private List<String> song = new ArrayList<>();
+    private String href = Objects.requireNonNull(this.getClass().getClassLoader().getResource("data.txt")).getFile();
+
+    public List<String> readSongText()
+    {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(href)))
+        {
+            String str;
+            while ((str = bufferedReader.readLine()) != null)
+            {
+                song.add(str);
+            }
+        } catch (IOException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        return song;}
+}
